a9a09f3eaf1d07abc02b1b3638ef0ccf85aef0ce
Bad0RANG3 <badorangeovo@outlook.com>
2026-03-28 01:46:38 +0800

    Initial Astro blog setup

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://bad0rang3.github.io',
+  base: '/',
+  output: 'static',
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
+});
